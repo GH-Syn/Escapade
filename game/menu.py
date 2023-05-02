1d44f9119e388fd4ccc308f7d924887bd0ae1af4
Joshua Rose <joshuarose099@gmail.com>
2023-05-02 21:36:54 +1000

    hotfix(ui): Fix runtime errors pertaining to loads
    [Description]:
    Fixes image loading issue in which image types are invalid for
    indexing.
    
    [Breaking Changes]:
    breaks any main menu interaction and visuals.
    
    [Tags/Labels]:
    hotfix

--- game/menu.py
+++ game/menu.py
@@ -1,13 +1,16 @@
-import re
 import os
 import pygame
 from pygame.constants import SRCALPHA
 
-from utils.pygame_ import fade_into_color
+
+# TODO: remove un-used imports
+import re # pyright: ignore
+from utils.pygame_ import fade_into_color  # pyright: ignore
 
 
 class Menu:
     window = pygame.display.get_surface()
+    pygame.font.init()
     font = os.path.join("res/fonts/Silkscreen", "silkscr.ttf")
 
     DEFAULT_MENU_BACKGROUND_COLOR = (25, 25, 25)
@@ -16,29 +19,33 @@ class Menu:
     dt = 0
     fps = 24
 
-    images_ = [i for i in os.listdir("res/menu")]
-    images = sorted(images_, key=lambda x: int(x.split("_")[-1].split(".")[0]))
-    scroll = [
-        pygame.image.load(os.path.join("res", "menu", i)).convert_alpha()
-        for i in images
-    ]
-
+    images_: list[str] = [i for i in os.listdir("res/menu")]
+    menu_sprites: list[pygame.SurfaceType] = []
+    
+    # prune image formats
+    for image in images_:
+        if not image.endswith(".png"):
+            images_.remove(image)
+            continue
+        print(image)
+        menu_sprites.append(pygame.image.load(os.path.join("res", "menu", image)).convert_alpha())
+    
     opening = True
     opening_frame = 0
     opening_tick = 0
-    image = scroll[0]
+    image = menu_sprites[0]
 
     mask = pygame.Surface(window.get_size(), SRCALPHA)
 
     @classmethod
     def open(cls):
-        """Open the scroll thing for the main menu"""
+        """Play main menu animation"""
         if not cls.opening:
             return
-        if cls.opening_frame < len(cls.scroll):
+        if cls.opening_frame < len(cls.menu_sprites):
             cls.opening_tick += 1
             if cls.opening_tick >= 3:
-                cls.image = cls.scroll[cls.opening_frame]
+                cls.image = cls.menu_sprites[cls.opening_frame]
                 cls.opening_tick = 0
                 cls.opening_frame += 1
         # blit mask
