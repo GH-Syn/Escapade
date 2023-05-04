693201bf9fc423f9d2d0097da09323e1834f6a47
Joshua Rose <joshuarose099@gmail.com>
2023-05-04 23:13:50 +1000

    feat: player class, bugfixes
    [Description]:
    Fixed gitignore somehow including .py in the gitignore. This is crazy,
    but anyhow the conf.py file for the documentation is now able to pass
    staging so i guess thats something.
    
    The player dunder files are ready in the entities class which basically
    has 0 functionality but all the properties and most of the physics are
    there. the only things that need to be addressed are filling out the
    physical methods themselves.
    
    [Breaking Changes]:
    No breaking changes noticed.
    
    [Related Issues/PRs]:
    ref: #18
    
    [Tags/Labels]:
    Feature
    Bugfix
    Bug
    Feat
    Enhancement

---
+++ entities/player.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.sprite import _Group
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,
+                 position: tuple[int | float, int | float],
+                 *groups: _Group) -> None:
+        super().__init__(*groups)
+
+        self.position = pygame.Vector2(*position)
+
+        self.accel = pygame.Vector2(0, 0)
+        self.velocity = pygame.Vector2(0, 0)
+
+        self.images = pygame.image.load
+
+        self.frame = 0
+        self.tick = 0
+        self.max_tick = 8
+
+        self.current_animation: str = "idle"
+
+    def animate(self):
+        pass
+
+    def move(self):
+        pass
+    
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def place(self):
+        pass
+
+    def attack(self):
+        pass
+
+    @property
+    def animation(self):
+        """ Current player animation """
+        return self._animation
+
+    @animation.setter
+    def animation(self, value):
+        self._animation = value
+
+    @animation.getter
+    def animation(self):
+        return self._animation
+
+    @property
+    def moving(self):
+        """ Returns true if velocity is greater than 0 """
+        return self._moving
+
+    @moving.setter
+    def moving(self, value: bool):
+        self._moving = value
+
+    @moving.getter
+    def moving(self):
+        return self.velocity.magnitude().__gt__(0)
+    
+    @property
+    def attacking(self):
+        """ Returns true if the player is in attacking animation/mode """
+        return self._attacking
+    
+    @attacking.setter
+    def attacking(self, value: bool):
+        self._attacking = value
+
+    @attacking.getter
+    def attacking(self):
+        return self.current_animation.__eq__("attack")
---
+++ game/zoom.py
@@ -0,0 +1,84 @@
+import json
+import sys
+
+import pygame
+
+
+class ZoomScreen:
+    datafile = open("data/theme.json", "r")
+    transition = json.load(datafile)["SplashScreen"]["default"]
+    datafile.close()
+
+    window = pygame.display.get_surface()
+
+    surface = scaled_surface = pygame.Surface(window.get_size(), pygame.SRCALPHA)
+
+    # Define the colors and fonts for the splash screen
+    background_color = transition["bg_color"]
+    text_color = transition["text_color"]
+
+    font_size = transition["font_size"]
+
+    # Get the center of the screen
+    center_x = window.get_width() // 2
+    center_y = window.get_height() // 2
+
+    winwidth = int(window.get_width())
+    winheight = int(window.get_height())
+
+    done = False
+
+    ZOOM_POINT = (740, 540)
+
+    # Define the fade in/out time in seconds
+    fade_time = transition["fade_time"] / 1000
+
+    final_scale_factor = 1500
+
+    # Define zoom animation
+    timer = pygame.time.Clock()
+    scale = 1
+    alpha = 255
+
+    image = pygame.image.load("res/menu/open_menu_1.png").convert_alpha()
+    surface.blit(image, (0, 0))
+
+    @classmethod
+    def update(cls):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+        if cls.scale < cls.final_scale_factor:
+            if cls.scale > 4:
+                if cls.alpha > 0:
+                    cls.alpha -= 25
+                    cls.scaled_surface.set_alpha(cls.alpha, pygame.SRCALPHA)
+                else:
+                    cls.done = True
+                    return False
+            if cls.scale < 7:
+                cls.scaled_surface = pygame.transform.scale(
+                    cls.scaled_surface,
+                    (cls.winwidth * cls.scale, cls.winheight * cls.scale),
+                )
+                cls.scaled_surface_rect = cls.scaled_surface.get_rect(
+                    center=(cls.center_x, cls.center_y)
+                )
+            else:
+                cls.done = True
+                return False
+            cls.scale += 0.1
+
+        cls.timer.tick(60)
+        return True
+
+    @classmethod
+    def draw(cls):
+        """Draw to screen as declared in class var"""
+
+        cls.scaled_surface.set_alpha(cls.alpha)
+        cls.window.blit(cls.scaled_surface, cls.scaled_surface_rect)
+
+        pygame.display.update()
